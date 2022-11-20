d9b69a17ca10957789345270ff2d7ed06a2c7614
Roman Kolesnyk <romakoles1@ukr.net>
2022-11-20 21:13:41 +0200

    error handler

--- src/main/java/com/kolesnyk/NftApplication.java
+++ src/main/java/com/kolesnyk/NftApplication.java
@@ -2,9 +2,10 @@ package com.kolesnyk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-//@EnableTransactionManagement
+@EnableTransactionManagement
 public class NftApplication {
     public static void main(String[] args) {
         SpringApplication.run(NftApplication.class, args);
--- src/main/java/com/kolesnyk/controller/global/ErrorHandler.java
+++ src/main/java/com/kolesnyk/controller/global/ErrorHandler.java
@@ -1,20 +1,43 @@
 package com.kolesnyk.controller.global;
 
 import com.kolesnyk.exception.EntityNotFound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({EntityNotFound.class, ConstraintViolationException.class})
-    public Map<String, String> entityValidation(RuntimeException e) {
+    @ExceptionHandler(EntityNotFound.class)
+    public Map<String, String> entityNotFound(EntityNotFound e) {
         return Map.of("code", "400",
-                "message", e.getMessage());
+                "message", e.getClass().getSimpleName());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> dtoValidation(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return Map.of("code", "400",
+                "message", message);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(Exception.class)
+    public Map<String, String> serverError(Exception e) {
+        LOGGER.error(e.getMessage());
+        return Map.of("code", "400",
+                "message", "Internal server error happened");
     }
 }
